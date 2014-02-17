deb7974eb878f5004e2bc2e145ceb2bc375b4d23
gaara <realwol@gmail.com>
2014-02-17 11:53:33 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '4.0.2'
+gem 'mysql2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IxcMall::Application.config.secret_key_base = '7c00b2e188b9a3c6335e24058d669b7ebf3a856234defddf3bd4b4b19073b0727263576ce2e7242527d96a4881a8a07b89e16be275c0833a1aa4d3e2c565625e'

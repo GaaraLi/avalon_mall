ed6602d609772af559c024e4adeffadd0a002cfc
gaara <realwol@gmail.com>
2014-02-19 17:43:56 +0800

    fix domain 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ IxcMall::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS.update(:session_domain => ".ixiangche.com")
+  config.action_controller.session :session_domain => '.ixiangche.com'
 end

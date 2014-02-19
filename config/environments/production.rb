2fddd7f7cca99fedb4b9761e7a604617bf6d6b22
gaara <realwol@gmail.com>
2014-02-19 18:19:58 +0800

    fix session domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,4 @@ IxcMall::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_controller.session :session_domain => '.ixiangche.com'
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-IxcMall::Application.config.session_store :cookie_store, key: '_ixc-mall_session'
+IxcMall::Application.config.session_store :cookie_store, key: '_ixc-mall_session', domain:'.ixiangche.com'
+

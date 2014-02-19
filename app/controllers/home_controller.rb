4b16a43f13afe52fc296c03aaedec77f2c2ce13c
gaara <realwol@gmail.com>
2014-02-19 17:20:13 +0800

    add session share

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def test
+  	@test= Customer.find(1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 IxcMall::Application.routes.draw do
   root "home#index"
+  get 'test' => 'home#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

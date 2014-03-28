81e37e9c4b1a02a1a81e0ea8a0f8e0b23f4b0cbc
gaara <realwol@gmail.com>
2014-03-28 10:55:57 +0800

    fix page 11

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,6 +13,8 @@ class HomeController < ApplicationController
 
     @card_area= MallBlock.where("mall_block_type_id=3").first
     @adv_area= MallBlock.where("mall_block_type_id=8").first
+    puts '=========='
+    puts @adv_area.img_url
 
     @nav_label_check= 1
   end

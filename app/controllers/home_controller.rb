0eda60b5df47db6f2c34e84b3549ad3659f62afb
gaara <realwol@gmail.com>
2014-04-19 14:41:28 +0800

    fix goods sorted by 3 cate; fix homepage slider; fix buy page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -35,23 +35,33 @@ class HomeController < ApplicationController
 
     @key= params[:search_key]
     @flag= params[:search_type].to_i
+    @first_show= 0
 
     if @flag==1
       @page_title="产品搜索"
       @goods= search_service(@key)
       @key_goods= @goods.page(params[:page])
+      @first_show= 1
     elsif @flag==2
-      @page_title="车行搜索"
+      @page_title="产品搜索"
       @vendors= search_vendor( @key)
       @page_vendors = @vendors.page(params[:page])
     elsif @flag==0
+      @page_title="全部商品"
       @goods=MallGood.all.onsale
       @key_goods= @goods.page(params[:page])
-      @page_title="全部商品"
+      @first_show= 0
     elsif @flag==3
+      @page_title="全部商品"
+      @goods= MallGood.all.onsale
+      @key_goods= @goods.page(params[:page])
+      @first_show= 3
+    elsif @flag==4
       @page_title="产品搜索"
       @goods= search_service_from_homepage(@key.to_i)
       @key_goods= @goods.page(params[:page])
+      @category_id = @key
+      @first_show= 0
     end
 
     @good_hot_rec= MallBlock.where("mall_block_type_id=15")
@@ -76,8 +86,108 @@ class HomeController < ApplicationController
     @second_class= MallCategory.where(:level=>2)
     @category_id= params[:id]
     @key= params[:key]
-    #@goods=MallGood.all
-    @goods=MallGood.where("title like '%#{@key}%' and mall_category_id= '#{@category_id}' ")
+    @flag= params[:order_flag]
+    @first_show= params[:first_show].to_i
+
+    #from anywhere when the click the category list
+    # from homepage left the first time 
+    if @first_show == 0
+      case @flag.to_i
+      when 1
+        @goods=MallGood.includes(:mall_skus).where(" mall_category_id= '#{@category_id}' ")
+        
+      when 2
+        @goods=MallGood.includes(:mall_skus).where(" mall_category_id= '#{@category_id}' ").order("mall_skus.sale_count ")
+        
+      when 3
+        @goods=MallGood.includes(:mall_skus).where(" mall_category_id= '#{@category_id}' ").order("mall_skus.show_price ")
+        
+      when 4
+        @goods=MallGood.includes(:mall_skus).where(" mall_category_id= '#{@category_id}' ").order("mall_skus.show_price DESC")
+        
+      else
+        @goods=MallGood.includes(:mall_skus).where(" mall_category_id= '#{@category_id}' ")
+        
+      end
+    #from top search label the first time
+    elsif @first_show ==1
+      case @flag.to_i
+      when 1
+        @goods=MallGood.includes(:mall_skus).where("title like '%#{@key}%' ")
+        
+      when 2
+        @goods=MallGood.includes(:mall_skus).where("title like '%#{@key}%' ").order("mall_skus.sale_count ")
+        
+      when 3
+        @goods=MallGood.includes(:mall_skus).where("title like '%#{@key}%' ").order("mall_skus.show_price ")
+        
+      when 4
+        @goods=MallGood.includes(:mall_skus).where("title like '%#{@key}%' ").order("mall_skus.show_price DESC")
+        
+      else
+        @goods=MallGood.includes(:mall_skus).where("title like '%#{@key}%' ")
+        
+      end
+    #all goods the first time
+    elsif @first_show ==2
+      case @flag.to_i
+      when 1
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      when 2
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.sale_count ")
+        
+      when 3
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price ")
+        
+      when 4
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price DESC")
+        
+      else
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      end
+    #all goods the first time
+    elsif @first_show ==3
+      case @flag.to_i
+      when 1
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      when 2
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.sale_count ")
+        
+      when 3
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price ")
+        
+      when 4
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price DESC")
+        
+      else
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      end
+=begin    # from homepage left the first time 
+    elsif @first_show ==4 
+      case @flag.to_i
+      when 1
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      when 2
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.sale_count DESC")
+        
+      when 3
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price DESC")
+        
+      when 4
+        @goods=MallGood.all.onsale.includes(:mall_skus).order("mall_skus.show_price")
+        
+      else
+        @goods=MallGood.all.onsale.includes(:mall_skus)
+        
+      end
+=end
+    end
+
     @key_goods= @goods.page(params[:page])
   end
 

4e365b98478a7cea1591cf9f24b7df8116fad9e6
solowang <wangchen19881218@126.com>
2014-05-14 20:11:50 -0700

    update

--- app/controllers/topic/topic_controller.rb
+++ app/controllers/topic/topic_controller.rb
@@ -4,14 +4,14 @@ class Topic::TopicController < ApplicationController
     #goods_id 72
     @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (90,91)");
     #goods_id 73
-    @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (92,93,94)");
+    @top2_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (92,93,94)");
     #goods_id 68
-    @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (85,86)");
+    @top3_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (85,86)");
     #goods_id 63
-    @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (79)");
+    @top4_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (79)");
     #goods_id 75
-    @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (96,97)");
+    @top5_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (96,97)");
     #goods_id 66
-    @top1_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (82)");
+    @top6_goods_qty = MallInventory.select(" ifnull(sum(inventory_qty),0) as sum_qty").where("mall_sku_id in (82)");
   end
 end
